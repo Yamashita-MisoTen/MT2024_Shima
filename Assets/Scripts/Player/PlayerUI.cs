c5494addac51e2f58cfdf6672d3bedf96ed60a78
Yamashita-MisoTen <YamashitaK.miso@gmail.com>
2023-11-28 03:32:15 +0900

    PC移行

--- Assets/Scripts/Player/PlayerUI.cs
+++ Assets/Scripts/Player/PlayerUI.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 using Mirror;
 using TMPro;
 using UnityEngine.UI;
+using System.Security;
+using Unity.VisualScripting;
 
 public class PlayerUI : NetworkBehaviour
 {
 	// Start is called before the first frame update
-	[SerializeField] GameObject UICanvasObj;
+	[SerializeField, Header("UI")] GameObject UICanvasObj;
+	[SerializeField] float chargeTime;
+	float requireChargeTime;
+	bool isCharge = false;
 	TextMeshProUGUI playerStateText;		// プレイヤーの状態を表示しておく
-	TextMeshProUGUI playerJumpStateText;
 	Image		playerHaveItemImage;
+	Image		SlideJumpImage;
+	Material	SlideJumpImageMaterial;
+	RectTransform	SlideJumpImageAnchoredPosition;
+	Image		HighJumpImage;
+	Material	HighJumpImageMaterial;
+	RectTransform	HighJumpImageAnchoredPosition;
 	void Start()
 	{
 		for(int i = 0; i < UICanvasObj.transform.childCount; i++){
@@ -19,8 +29,21 @@ public class PlayerUI : NetworkBehaviour
 			if(childObj.name == "PlayerState"){
 				playerStateText = childObj.GetComponent<TextMeshProUGUI>();
 			}
-			if(childObj.name == "JumpState"){
-				playerJumpStateText = childObj.GetComponent<TextMeshProUGUI>();
+			if(childObj.name == "SlideJumpImage"){
+				SlideJumpImage = childObj.GetComponent<Image>();
+				SlideJumpImageMaterial = SlideJumpImage.material;
+				SlideJumpImageAnchoredPosition = childObj.GetComponent<RectTransform>();
+				Debug.Log(SlideJumpImage);
+				Debug.Log(SlideJumpImageMaterial);
+				Debug.Log(SlideJumpImageAnchoredPosition);
+			}
+			if(childObj.name == "HighJumpImage"){
+				HighJumpImage = childObj.GetComponent<Image>();
+				HighJumpImageMaterial = HighJumpImage.material;
+				HighJumpImageAnchoredPosition = childObj.GetComponent<RectTransform>();
+				Debug.Log(HighJumpImage);
+				Debug.Log(HighJumpImageMaterial);
+				Debug.Log(HighJumpImageAnchoredPosition);
 			}
 		}
 		UICanvasObj.SetActive(false);
@@ -30,7 +53,18 @@ public class PlayerUI : NetworkBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+		if(isCharge){
+			float ratio = requireChargeTime / chargeTime;
+			HighJumpImageMaterial.SetFloat("_Ratio",Mathf.Lerp(1f,0f,ratio));
 
+			requireChargeTime += Time.deltaTime;
+			if(chargeTime < requireChargeTime){
+				requireChargeTime = 0f;
+				HighJumpImageMaterial.SetFloat("_Ratio",1.1f);
+				HighJumpImageMaterial.SetInt("_isCharge",0);
+				isCharge = false;
+			}
+		}
 	}
 
 	public void ChangeOrgaPlayer(bool orgaflg){
@@ -42,7 +76,10 @@ public class PlayerUI : NetworkBehaviour
 	}
 
 	public void ChangeJumpType(CPlayer.eJump_Type jumptype){
-		playerJumpStateText.text = jumptype.ToString();
+		if(jumptype == CPlayer.eJump_Type.UP){
+		}else if(jumptype == CPlayer.eJump_Type.SIDE){
+
+		}
 	}
 
 	public void MainSceneUICanvas(){
@@ -55,6 +92,12 @@ public class PlayerUI : NetworkBehaviour
 			Debug.Log("PlayerUI_Off");
 			UICanvasObj.SetActive(false);
 		}
+	}
+
+	public void SetCharge(){
+		isCharge = true;
+		HighJumpImageMaterial.SetFloat("_Ratio",0f);
+		HighJumpImageMaterial.SetInt("_isCharge",1);
 	}
 
 	public void SetActiveUICanvas(bool flg){
