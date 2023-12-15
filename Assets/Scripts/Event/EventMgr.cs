9a2afb25024615884a448d53489eb609884461a1
Yamashita-MisoTen <YamashitaK.miso@gmail.com>
2023-12-16 04:35:25 +0900

    UI完成

--- Assets/Scripts/Event/EventMgr.cs
+++ Assets/Scripts/Event/EventMgr.cs
@@ -9,9 +9,33 @@ public class EventMgr : NetworkBehaviour
 	[SerializeField] List<GameEvent> eventKind;
 	[SerializeField] GameObject eventUIObj;
 	[SerializeField] EventUI eventUI;
+	GameEvent eventObj;
+	float eventTime = 0f;
+	float requireEventTime = 0f;
+	bool isEventNow = false;
 	void Start(){
 		eventUI = Instantiate(eventUIObj).GetComponent<EventUI>();
 	}
+	void Update(){
+		if(!isServer) return;
+		if(!isEventNow) return;
+
+		requireEventTime += Time.deltaTime;
+		if(requireEventTime >= eventTime){
+			eventObj.FinishEvent();
+			requireEventTime = 0f;
+			eventUI.StopEventBeltUI();
+			eventObj =null;
+		}
+		var ratio = requireEventTime / eventTime;
+		RpcSetRatio(ratio);
+	}
+
+	[ClientRpc]
+	void RpcSetRatio(float ratio){
+		eventUI.SetRatio(ratio);
+	}
+
 	public void SetUpUI(GameObject playerObj){
 		for(int i = 0 ; i < playerObj.transform.childCount; i++){
 			var obj = playerObj.transform.GetChild(i);
@@ -25,19 +49,21 @@ public class EventMgr : NetworkBehaviour
 	public void RandomEventLottery(){
 		if(eventKind == null) return;
 
-		StartPerformance();
-
 		var eventnum = Random.Range(0, eventKind.Count);
 		Debug.Log(eventKind[eventnum].GetEventName() + "発生");
 		if(isServer) {
-			var obj = Instantiate(eventKind[eventnum]);
-			NetworkServer.Spawn(obj.gameObject);
-			obj.StartEvent();
+			eventObj = Instantiate(eventKind[eventnum]);
+			NetworkServer.Spawn(eventObj.gameObject);
+			eventObj.StartEvent();
+			eventTime = eventObj.eventTime();
+			isEventNow = true;
 		}
+
+		StartPerformance(eventKind[eventnum].GetEventName(), eventKind[eventnum].GetExplanatory());
 	}
 
-	void StartPerformance(){
-		eventUI.StartUpEventUI();
+	void StartPerformance(string name, string explanatory){
+		eventUI.StartUpEventUI(name, explanatory);
 		DOVirtual.DelayedCall(3f,() => eventUI.StopEventUI());
 	}
 }
